1f64eeff43d222cdfbf8a41926ec44d2b82bb5c7
sambitarai17@gmi17@§§§ls <sambitarai17@gmail.com>
2023-03-17 11:16:48 +0100

    make_dirs function added (This will create all the required directories for output)

--- utils.py
+++ utils.py
@@ -1,2 +1,15 @@
 import os
 
+def make_dirs(path, k):
+	path_CV = os.path.join(path, "CV_" + str(k))
+	if not os.path.exists(path_CV):
+		os.mkdir(path_CV)
+	path_Network_weights = os.path.join(path_CV, "Network_Weights")
+	if not os.path.exists(path_Network_weights):
+		os.mkdir(path_Network_weights)
+	path_MIPs = os.path.join(path_CV, "MIPs")
+	if not os.path.exists(path_MIPs):
+		os.mkdir(path_MIPs)
+	path_Metrics = os.path.join(path_CV, "Metrics")
+	if not os.path.exists(path_Metrics):
+		os.mkdir(path_Metrics)
