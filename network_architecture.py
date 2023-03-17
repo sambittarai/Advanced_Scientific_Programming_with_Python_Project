7a352e31ef1207a96a71973f45efe586d7a22dd2
sambitarai17@gmi17@§§§ls <sambitarai17@gmail.com>
2023-03-17 09:06:09 +0100

    Network architecture created

--- network_architecture.py
+++ network_architecture.py
@@ -1,1 +1,16 @@
-import os
+from config import parse_args
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+
+def build_network(args, device):
+	model = UNet(
+				dimensions=args.dimensions,
+				in_channels=args.in_channels,
+				out_channels=args.out_channels,
+				channels=args.kernel_channels,
+				strides=args.strides,
+				num_res_units=args.residual_units,
+				norm=Norm.BATCH,
+				dropout=args.dropout,
+			).to(device)
+	return model
